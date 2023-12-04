b28c14f88b74b1677283454e8f88e74c5cd520dc
Josephine Adebayo <josephinadebayo84@gmail.com>
2023-12-04 13:40:14 -0400

    Implementing feedback section

--- app.py
+++ app.py
@@ -23,6 +23,27 @@ from torchvision import transforms
 # linearNet = LinearpyTeen.PyTeen()
 ConvoNet = SimpleConvoModel.PyTeen()
 
+def save_failed_guess(image_data):
+    try:
+        # Create a folder to store failed guesses if it doesn't exist
+        folder_path = "failed_guesses"
+        os.makedirs(folder_path, exist_ok=True)
+
+        # Generate a unique filename using UUID
+        image_filename = f"{uuid.uuid4()}.png"
+        image_path = os.path.join(folder_path, image_filename)
+
+        # Save the image to the specified folder
+        cv.imwrite(image_path, image_data)
+
+        print(f"Image successfully saved at: {image_path}")
+
+    except Exception as e:
+        # Handle any exceptions or errors that might occur
+        print(f"Error occurred while saving the image: {str(e)}")
+        # Optionally, you can raise the exception to propagate it further
+        raise e
+
 # function main contains the code for the main page
 def main():
     if "button_id" not in st.session_state:
@@ -40,7 +61,7 @@ def full_app():
     st.sidebar.title("Configuration")
     st.markdown(
         """
-    Draw a number from **0 to 9** and click on the guess button!
+    Draw a number from **0 to 9** and click on the AI Guess button!
     * Go easy at first, then try to fool it
     * We also appreciate feedback! :)
     """
@@ -71,26 +92,22 @@ def full_app():
         key="full_app",
     )
 
-    if st.button(label="AI Guess \U0001F914", type = "primary"):
+    if st.button(label="AI Guess \U0001F914", type="primary"):
         ConvoNet.load_state_dict(torch.load('pyTeenConvo9882.pth'))
-        # st.image(canvas_result.image_data)
-        cv.imwrite("canvas.png",  canvas_result.image_data)
+        cv.imwrite("canvas.png", canvas_result.image_data)
 
-        # this section of the code does the image manipulation
-        # before passing it to our NN for guessing
-        img = cv.imread("canvas.png",cv.IMREAD_GRAYSCALE)
-        scaling = cv.resize(img,(28,28))
-        my_img_processed = cv.bitwise_not(scaling) # inverting image (background needs to be black)
-        cv.imwrite('new.png',my_img_processed)
-        new_img = Image.open("new.png")
-        # st.image(new_img)
-        # my_img_transform = transforms.Compose([transforms.Resize((28,28)),transforms.ToTensor()])
-        # input_img = LinearpyTeen.My_transform(my_img_processed).unsqueeze(0) # converting it to a batch
+        img = cv.imread("canvas.png", cv.IMREAD_GRAYSCALE)
+        scaling = cv.resize(img, (28, 28))
+        my_img_processed = cv.bitwise_not(scaling)
+        cv.imwrite('new.png', my_img_processed)
         input_img = SimpleConvoModel.my_transform(my_img_processed).unsqueeze(0)
-        # guessed_digit = linearNet.predict(input_img)
         guessed_digit = ConvoNet.predict(input_img)
         ans = str(guessed_digit.item())
         st.subheader(f"The AI guessed that it was a {ans}\n")
+
+        if ans == "Could not guess":
+            save_failed_guess(canvas_result.image_data)
+            st.success("Image saved for further analysis.")
 
     # Do something interesting with the image data and paths
     # if canvas_result.image_data is not None:
@@ -105,13 +122,10 @@ def full_app():
 
 
 if __name__ == "__main__":
-
     st.set_page_config(
-        page_title="Guessing Number Demo", page_icon=":pencil2:",
+        page_title="Digit Guessing App", page_icon=":pencil2:",
         initial_sidebar_state="collapsed"
     )
     st.title("AI trying its best to guess a number!")
 
     main()
-
-
