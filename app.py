e88ac7ce2a9bbdf28d8180aa0137a15449f960c0
Josephine Adebayo <josephinadebayo84@gmail.com>
2023-12-04 20:55:08 +0000

    implementing the feedback button

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ import os
 import re
 import time
 import uuid
+import shutil
 from io import BytesIO
 from pathlib import Path
+from datetime import datetime
 
 import numpy as np
 import pandas as pd
@@ -24,16 +26,38 @@ from torchvision import transforms
 ConvoNet = SimpleConvoModel.PyTeen()
 
 # function main contains the code for the main page
-def main():
-    if "button_id" not in st.session_state:
-        st.session_state["button_id"] = ""
-    if "color_to_label" not in st.session_state:
-        st.session_state["color_to_label"] = {}
+def main(canvas_result):
+    if st.button(label="AI Guess \U0001F914", type="primary"):
+        ConvoNet.load_state_dict(torch.load('pyTeenConvo9882.pth'))
+        cv.imwrite("canvas.png", canvas_result.image_data)
 
-    full_app()
+        img = cv.imread("canvas.png", cv.IMREAD_GRAYSCALE)
+        scaling = cv.resize(img, (28, 28))
+        my_img_processed = cv.bitwise_not(scaling)
+        cv.imwrite('new.png', my_img_processed)
 
-    with st.sidebar:
-        st.markdown("---")
+        # Processing the image for model prediction
+        input_img = SimpleConvoModel.my_transform(my_img_processed).unsqueeze(0)
+        guessed_digit = ConvoNet.predict(input_img)
+        ans = str(guessed_digit.item())
+        st.subheader(f"The AI guessed that it was a {ans}\n")
+
+        user_feedback = st.checkbox("AI Guess Incorrect?")
+        if user_feedback:
+            # Create a folder for incorrect guesses if it doesn't exist
+            incorrect_folder = 'incorrect_guesses'
+            os.makedirs(incorrect_folder, exist_ok=True)
+
+            # Generate a unique folder name using timestamp
+            folder_name = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            folder_path = os.path.join(incorrect_folder, folder_name)
+            os.makedirs(folder_path, exist_ok=True)
+
+            # Save canvas.png and new.png in the created folder
+            shutil.copy("canvas.png", os.path.join(folder_path, "canvas.png"))
+            shutil.copy("new.png", os.path.join(folder_path, "new.png"))
+
+            st.success("Feedback received, thank you!")
 
 # full_app function corresponds to the canvas found in a public github repo
 def full_app():
@@ -70,27 +94,7 @@ def full_app():
         display_toolbar=st.sidebar.checkbox("Display toolbar", True),
         key="full_app",
     )
-
-    if st.button(label="AI Guess \U0001F914", type = "primary"):
-        ConvoNet.load_state_dict(torch.load('pyTeenConvo9882.pth'))
-        # st.image(canvas_result.image_data)
-        cv.imwrite("canvas.png",  canvas_result.image_data)
-
-        # this section of the code does the image manipulation
-        # before passing it to our NN for guessing
-        img = cv.imread("canvas.png",cv.IMREAD_GRAYSCALE)
-        scaling = cv.resize(img,(28,28))
-        my_img_processed = cv.bitwise_not(scaling) # inverting image (background needs to be black)
-        cv.imwrite('new.png',my_img_processed)
-        new_img = Image.open("new.png")
-        # st.image(new_img)
-        # my_img_transform = transforms.Compose([transforms.Resize((28,28)),transforms.ToTensor()])
-        # input_img = LinearpyTeen.My_transform(my_img_processed).unsqueeze(0) # converting it to a batch
-        input_img = SimpleConvoModel.my_transform(my_img_processed).unsqueeze(0)
-        # guessed_digit = linearNet.predict(input_img)
-        guessed_digit = ConvoNet.predict(input_img)
-        ans = str(guessed_digit.item())
-        st.subheader(f"The AI guessed that it was a {ans}\n")
+    return canvas_result
 
     # Do something interesting with the image data and paths
     # if canvas_result.image_data is not None:
@@ -111,7 +115,5 @@ if __name__ == "__main__":
         initial_sidebar_state="collapsed"
     )
     st.title("AI trying its best to guess a number!")
-
-    main()
-
-
+    canvas_result = full_app()
+    main(canvas_result)
